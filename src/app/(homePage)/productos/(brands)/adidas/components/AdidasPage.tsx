c404d4ae765a6e5a40cffa904687ea0ee7326124
DavidMoralesz4 <jd3285201@gmail.com>
2024-11-27 22:36:17 -0500

    brands-csv

--- src/app/(homePage)/productos/(brands)/adidas/components/AdidasPage.tsx
+++ src/app/(homePage)/productos/(brands)/adidas/components/AdidasPage.tsx
@@ -1,39 +1,50 @@
 "use client";
 
-import Papa from "papaparse";
+import React, { useState, useContext } from "react";
 import { Product } from "@/interfaces/typesModal";
 import SearchBar from "@/components/searchBar/SearchBar";
 import Link from "next/link";
 import { AppContext } from "@/context/modalContext";
 import { TypesModal } from "@/interfaces/typesModal";
-import React, { useState, useContext } from "react";
 import AdidasCards from "./AdidasCards";
 import ButtonIU from "@/components/buttonIU/ButtonIU";
 import FormUploaderExcel from "@/components/form-uploader-excel/FormUploaderExcel";
+import axios from "axios";
 
 export default function AdidasPage() {
-  const [products, setProducts] = useState<Product[]>([]);
-
+  // const [products, setProducts] = useState<Product[]>([]);
+  const [file, setFile] = useState<File | null>(null);
+  const [message, setMessage] = useState("");
+  
   const { excelModalForm, onClose, setExcelModalForm } = useContext(
     AppContext
   ) as TypesModal;
 
-  const readFileCsv = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
 
-    if (file) {
-      Papa.parse<Product>(file, {
-        header: true,
-        skipEmptyLines: true,
-        complete: (response) => {
-          setProducts(response.data);
-        },
-        error: (err) => {
-          console.error("Error al leer el archivo:", err);
-        },
-      });
+    if(!file) {
+      setMessage("Por favor seleciona un archivo CSV.")
+      return
     }
-  };
+    
+    const formData = new FormData()
+    formData.append("file", file)
+
+    try {
+      const response = axios.post("http://localhost:4001/api/brand/6747a0d7fcae916239798b0d/products/csv", formData)
+      setMessage(response.data.message)
+    } catch (error){
+      console.error("Ha ocurrido un error", error)
+    } 
+  }
+
+  const readFileCsv = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if(event.target.files && event.target.files[0]){
+      setFile(event.target.files[0])
+    }
+
+  }
 
   return (
     <>
@@ -49,7 +60,7 @@ export default function AdidasPage() {
         </div>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
-        {products.map((product, index) => (
+        {/* {products.map((product, index) => (
           <AdidasCards
             key={index}
             name={product.name}
@@ -59,7 +70,7 @@ export default function AdidasPage() {
             price={product.price}
             size={product.size}
           />
-        ))}
+        ))} */}
       </div>
       <FormUploaderExcel
         onClose={onClose}
--- src/components/form-uploader-excel/FormUploaderExcel.tsx
+++ src/components/form-uploader-excel/FormUploaderExcel.tsx
@@ -10,7 +10,7 @@ function FormUploaderExcel({ excelModalForm, onClose, readFileCsv }: HandleCance
 
   return (
     <div className="fixed top-0 right-0 bottom-0 left-0 bg-black bg-opacity-40 flex justify-center items-center">
-      <div className="bg-[#0d1111] p-10 mt-5 rounded-md relative">
+      <form className="bg-[#0d1111] p-10 mt-5 rounded-md relative">
         <button
           onClick={() => onClose()}
           className="absolute top-2 right-2 text-white hover:text-red-500 focus:outline-none w-8 h-8 flex items-center justify-center"
@@ -67,7 +67,7 @@ function FormUploaderExcel({ excelModalForm, onClose, readFileCsv }: HandleCance
             {" " + statusMessage}
           </div>
         )} */}
-      </div>
+      </form>
     </div>
   );
 }
